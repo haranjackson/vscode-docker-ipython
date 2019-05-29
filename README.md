cdf96ffaba177289eb47574b370bd3935fec2796
Haran Jackson <jackson.haran@gmail.com>
2019-05-29 18:12:35 +0100

    initial code commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# vscode-docker-ipython
-A Visual Studio Code extension: Develop interactively with an IPython terminal, running from a locally-defined docker image
+## Description
+
+Say you have a DockerFile in your current directory, specifying an environment under which you want Python to run.
+This extension allows you to develop and run code interactively under this environment.
+
+This extension is build upon the work of pancho111203: github.com/pancho111203/vscode-ipython
+
+Note that ~/.aws is mapped to /root/.aws in the Docker container (so that you can access your AWS credentials, if you would like to).
+
+## Usage
+
+There are two commands:
+
+- docker-ipython.sendFileContentsToIPython
+  Sends the complete file contents into the open Python instance (or a new one if none is open)
+
+- docker-ipython.sendSelectedToIPython
+  Sends the selected lines, or the one where the cursor is, to the open Python instance (or a new one if none is open)
+
+
+## Limitations
+
+Can't have multiple instances running simultaneously.
---
+++ extension.js
@@ -0,0 +1,112 @@
+const uuidv4 = require('uuid/v4');
+const vscode = require('vscode');
+
+const helpers = require('./helpers');
+
+const pythonTerminalName = 'Docker-IPython';
+let pythonTerminal = null;
+let textQueue = [];
+let waitsQueue = [];
+let currentFilename = null;
+
+function createPythonTerminal() {
+    textQueue = [];
+    waitsQueue = [];
+    pythonTerminal = vscode.window.createTerminal(pythonTerminalName);
+    var uuid = uuidv4();
+    sendQueuedText(`docker build . -t ${uuid}`, 1500);
+    sendQueuedText(`docker run -v \${HOME}/.aws:/root/.aws:ro -v \${PWD}:/docker-ipython -w /docker-ipython -e DIP_PWD=\${PWD} -it ${uuid} /bin/bash`, 1500);
+    sendQueuedText('pip install ipython', 1500);
+    sendQueuedText('ipython', 1500);
+    sendQueuedText('import os');
+    sendQueuedText('DIP_PWD = os.environ["DIP_PWD"]');
+}
+
+function removePythonTerminal() {
+    pythonTerminal = null;
+    currentFilename = null;
+    textQueue = [];
+    waitsQueue = [];
+}
+
+function sendQueuedText(text, waitTime = 50) {
+    textQueue.push(text);
+    waitsQueue.push(waitTime);
+}
+
+function queueLoop() {
+    if (textQueue.length > 0 && pythonTerminal !== null && pythonTerminal._queuedRequests.length === 0) {
+        const text = textQueue.shift();
+        const waitTime = waitsQueue.shift();
+        pythonTerminal.sendText(text);
+        setTimeout(queueLoop, waitTime);
+    } else {
+        setTimeout(queueLoop, 50);
+    }
+}
+
+function updateFilename(filename) {
+    currentFilename = filename;
+    sendQueuedText(`__file__ = '${filename}'.replace(DIP_PWD, '')`);
+    sendQueuedText('if __file__.startswith("/"): __file__ = __file__[1:]')
+}
+
+
+function activate(context) {
+    vscode.window.onDidCloseTerminal(function (event) {
+        if (event._name === pythonTerminalName) {
+            removePythonTerminal();
+        }
+    });
+
+    queueLoop();
+
+    let sendSelectedToIPython = vscode.commands.registerCommand('docker-ipython.sendSelectedToIPython', function () {
+        if (pythonTerminal === null) {
+            createPythonTerminal();
+        }
+        const editor = vscode.window.activeTextEditor;
+        const filename = editor.document.fileName;
+        if (filename !== currentFilename) {
+            updateFilename(filename);
+        }
+
+        let startLine, endLine;
+        if (editor.selection.isEmpty) {
+            startLine = editor.selection.active.line + 1
+            endLine = startLine;
+        } else {
+            startLine = editor.selection.start.line + 1;
+            endLine = editor.selection.end.line + 1;
+        }
+
+        const command = `%load -r ${startLine}-${endLine} $__file__`;
+        sendQueuedText(command);
+        sendQueuedText('\n\n');
+        pythonTerminal.show();
+    });
+
+    let sendFileContentsToIPython = vscode.commands.registerCommand('docker-ipython.sendFileContentsToIPython', function () {
+        if (pythonTerminal === null) {
+            createPythonTerminal();
+        }
+
+        const editor = vscode.window.activeTextEditor;
+        const filename = editor.document.fileName;
+        if (filename !== currentFilename) {
+            updateFilename(filename);
+        }
+
+        sendQueuedText('%load $__file__', 100);
+        sendQueuedText('\n\n');
+        pythonTerminal.show();
+    });
+
+    context.subscriptions.push(sendSelectedToIPython);
+    context.subscriptions.push(sendFileContentsToIPython);
+}
+exports.activate = activate;
+
+function deactivate() {
+}
+exports.deactivate = deactivate;
